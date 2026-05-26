13bab9a7335d418c2f7b725a7d5eb9d626deaf39
cherifcoder@gmail.com <cherifcoder@gmail.com>
2026-05-26 16:17:05 +0200

    ajout des formulaires

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI_LOCAL, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
     });
     console.log(` MongoDB connecté : ${conn.connection.host}`);
   } catch (err) {
